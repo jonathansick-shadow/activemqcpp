a2bfbae3876a08826481ffad7d6ed0bf1a610e87
Mario Juric <mjuric@lsst.org>
2013-12-17 01:45:49 -0600

    Initial import of activemqcpp-3.5.0

---
+++ ups/eupspkg.cfg.sh
@@ -0,0 +1,10 @@
+# EupsPkg config file. Sourced by 'eupspkg'
+
+CONFIGURE_OPTIONS="--prefix=$PREFIX --with-apr=$APR_DIR --with-apr-util=$APR_UTIL_DIR --enable-doxygen-man"
+MAKE_BUILD_TARGETS="all doxygen-run"
+
+install()
+{
+	default_install "$@"
+	cp -a doc "$PREFIX"
+}
